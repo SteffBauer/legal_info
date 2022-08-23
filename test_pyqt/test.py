41148b7486222b8125c75f7674b0ab4679e8868a
Stefan Bauer <stefan.bauer08@web.de>
2022-08-23 15:20:47 +0200

    Start with evaluating pyqt

---
+++ test_pyqt/test.py
@@ -0,0 +1,17 @@
+from PyQt5 import QtWidgets
+from PyQt5.QtWidgets import QApplication, QMainWindow
+import sys
+
+
+def window():
+    app = QApplication(sys.argv)
+    win = QMainWindow()
+    win.setGeometry(400, 400, 300, 300)
+    win.setWindowTitle("PyQt5 Test")
+
+    win.show()
+    sys.exit(app.exec_())
+
+
+if __name__ == "__main__":
+    window()
