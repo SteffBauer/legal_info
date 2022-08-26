d6dfee749de05fcb7acb6d87316c7a116c4e8046
Stefan Bauer <stefan.bauer08@web.de>
2022-08-26 13:43:21 +0200

    Update python test file

--- test_pyqt/test.py
+++ test_pyqt/test.py
@@ -1,19 +1,28 @@
-from PyQt5 import QtWidgets
 from PyQt5.QtWidgets import QApplication, QMainWindow
+import PyQt5.QtWidgets as widgets
 import sys
 
 
 def window():
-    app = QApplication(sys.argv)
     win = QMainWindow()
     win.setGeometry(400, 400, 300, 300)
     win.setWindowTitle("PyQt5 Test")
-    lbl = QtWidgets.QLabel(win)
-    lbl.setText("Hello World")
+    return win
 
-    win.show()
-    sys.exit(app.exec_())
+
+def createView(win):
+    label = widgets.QLabel(win)
+    label.setText("Hello World")
+    text = widgets.QTextEdit(win)
+    text.setText("This is a text edit")
+    text.setGeometry(0, 25, 150, 25)
 
 
 if __name__ == "__main__":
-    window()
+    app = QApplication(sys.argv)
+    win = window()
+
+    createView(win)
+
+    win.show()
+    sys.exit(app.exec_())
