8b05e01443f803bf695a64a0fac7bc58c7f04424
Stefan Bauer <stefan.bauer08@web.de>
2022-09-03 14:07:44 +0200

    Work on views

--- lib/views/tools/modules/calc_alc.dart
+++ lib/views/tools/modules/calc_alc.dart
@@ -16,7 +16,35 @@ class CalculateAlcPage extends StatefulWidget implements IViewDetailModel {
 }
 
 class _CalculateAlcPageState extends State<CalculateAlcPage> {
+  final _controllerHeight = TextEditingController();
+  final _controllerWheight = TextEditingController();
+
   GenderType? _gender = GenderType.male;
+  var _weight = 80.0;
+  var _height = 180;
+
+  @override
+  void initState() {
+    super.initState();
+
+    _controllerHeight.text = _height.toString();
+    _controllerHeight.selection =
+        TextSelection(baseOffset: 0, extentOffset: _height.toString().length);
+    _controllerHeight.value = TextEditingValue(
+      text: _controllerHeight.text,
+      selection: _controllerHeight.selection,
+      composing: TextRange.empty,
+    );
+
+    _controllerWheight.text = _weight.toString();
+    _controllerWheight.selection =
+        TextSelection(baseOffset: 0, extentOffset: _weight.toString().length);
+    _controllerWheight.value = TextEditingValue(
+      text: _controllerWheight.text,
+      selection: _controllerWheight.selection,
+      composing: TextRange.empty,
+    );
+  }
 
   @override
   Widget build(BuildContext context) {
@@ -30,6 +58,7 @@ class _CalculateAlcPageState extends State<CalculateAlcPage> {
         Radio(
           value: genderType,
           groupValue: _gender,
+          activeColor: Theme.of(context).primaryColor,
           onChanged: (GenderType? value) {
             setState(() {
               _gender = value;
@@ -65,29 +94,38 @@ class _CalculateAlcPageState extends State<CalculateAlcPage> {
               )
             ]),
             SizedBox(height: spaceBetweenFields),
-            TextField(
+            TextFormField(
               decoration: const InputDecoration(
                 labelText: "Gewicht in kg",
               ),
               keyboardType: TextInputType.number,
+              controller: _controllerWheight,
+              onTap: () {
+                _controllerWheight.selection = TextSelection(
+                    baseOffset: 0,
+                    extentOffset: _controllerWheight.text.length);
+              },
               onChanged: (String value) {
-                // todo set value
-                if (kDebugMode) {
-                  print(value);
-                }
+                setState(() {
+                  _weight = double.tryParse(value) ?? 0.0;
+                });
               },
             ),
             SizedBox(height: spaceBetweenFields),
-            TextField(
+            TextFormField(
               decoration: const InputDecoration(
                 labelText: "Körpergröße in cm",
               ),
               keyboardType: TextInputType.number,
+              controller: _controllerHeight,
+              onTap: () {
+                _controllerHeight.selection = TextSelection(
+                    baseOffset: 0, extentOffset: _controllerHeight.text.length);
+              },
               onChanged: (String value) {
-                // todo set value
-                if (kDebugMode) {
-                  print(value);
-                }
+                setState(() {
+                  _height = int.tryParse(value) ?? 0;
+                });
               },
             ),
             SizedBox(height: spaceBetweenFields),
@@ -104,6 +142,16 @@ class _CalculateAlcPageState extends State<CalculateAlcPage> {
               },
             ),
             SizedBox(height: spaceBetweenFields),
+            SizedBox(height: spaceBetweenFields),
+            ElevatedButton(
+                onPressed: (() {
+                  // todo calculate
+                  if (kDebugMode) {
+                    print("Höhe: $_height");
+                    print("Gewicht: $_weight");
+                  }
+                }),
+                child: Text("Berechnen"))
           ],
         ),
       ),
